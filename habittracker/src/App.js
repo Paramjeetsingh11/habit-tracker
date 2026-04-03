c624c0a1a69328c6c95326e4e73ecf5893663a77
Paramjeetsingh11 <paramjeetsinghgulati03@gmail.com>
2026-04-03 15:20:35 +0530

    "habittracker project setup"

---
+++ habittracker/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Habit Tracker</h1>
+      <p>Welcome to Habit Tracker App</p>
+    </div>
+  );
+}
+
+export default App;
